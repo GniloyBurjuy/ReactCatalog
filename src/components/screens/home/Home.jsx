b26c62aea0a52a89b251974bfadec9b7840f0ee2
burjuy <sample@mail.ru>
2023-06-30 20:51:42 +0300

    first commit

---
+++ src/components/screens/home/Home.jsx
@@ -0,0 +1,18 @@
+import styles from './Home.module.css'
+
+function Home() {
+    return (
+        <div>
+          <h1>Cars catalog</h1>
+          <div>
+            <div className={styles.item}>
+                <h2>Car 1</h2>
+                <p>$100 000</p>
+                <button>Read more</button>
+            </div>
+          </div>
+        </div>
+    )
+  }
+  
+  export default Home
